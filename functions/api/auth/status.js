ca95bfeddb2c8c7adc39f82ced90b630ce901219
Matheus Vinicius Landucci Neres <snakerr77@gmail.com>
2026-08-07 09:26:09 -0300

    Delete functions directory

--- functions/api/auth/status.js
+++
@@ -1,5 +0,0 @@
-import { json } from "../../_lib/auth.js";
-export async function onRequestGet(context) {
-  const row = await context.env.DB.prepare("SELECT COUNT(*) AS total FROM users WHERE active = 1").first();
-  return json({ configured: Number(row?.total || 0) > 0 });
-}
